5d66dfdbde03289c72dc1092e814137f8661ac40
PilarczykM <marcinpilarczyk@me.com>
2022-06-27 14:05:18 +0200

    Update game.py
    use new RULES system

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from scoreboard import Scoreboard
-from rules import Rules
+from rules import RULES, get_winner
 from user_interface.ui import UI
 
 
@@ -11,7 +11,7 @@ class Game:
         self.ui = ui
         self.scoreboard = Scoreboard()
         self.max_round = max_round
-        self.rules = Rules()
+        self.rules = RULES
         self.user: str = user
         self.cpu: str = "cpu"
 
@@ -30,7 +30,7 @@ class Game:
             self.ui.display_tie()
             return
 
-        winner, message = self.rules.get_winner(user_entity, cpu_entity)
+        winner, message = get_winner(user_entity, cpu_entity)
         if winner == user_entity:
             self.ui.display_round_winner(self.user, user_entity, message)
             self.scoreboard.points[self.user] += 1
@@ -40,6 +40,6 @@ class Game:
 
     def play(self):
         self.ui.display_rules()
-        for i in range(self.max_round):
+        for _ in range(self.max_round):
             self._do_turn()
             self.ui.display_score(self.scoreboard.points)
