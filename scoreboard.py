a8c9f596f25d773076264734ae45051e8aeff5d8
PilarczykM <marcinpilarczyk@me.com>
2022-06-27 14:34:37 +0200

    Update scoreboard.py
    Add win round method

--- scoreboard.py
+++ scoreboard.py
@@ -7,7 +7,10 @@ class Scoreboard:
     """
 
     def __init__(self):
-        self.points: Dict[str, int] = defaultdict(int)
+        self.points: dict[str, int] = {}
 
-    def register_player(self, user_name: str) -> None:
-        self.points[user_name] = 0
+    def register_player(self, player_name: str) -> None:
+        self.points[player_name] = 0
+
+    def win_round(self, player_name: str) -> None:
+        self.points[player_name] += 1
