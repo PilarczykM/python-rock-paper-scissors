2c7e220ff2946a5b023f07646bcfef10c41573e3
PilarczykM <marcinpilarczyk@me.com>
2022-06-27 10:54:24 +0200

    Create scoreboard.py
    Add scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from typing import Dict
+
+
+class Scoreboard:
+    """Scoreboard class to register player, track points and display the card
+    """
+
+    def __init__(self):
+        self.points: Dict[str, int] = defaultdict(int)
+
+    def register_player(self, user_name: str) -> None:
+        self.points[user_name] = 0
+
+    def display_score(self):
+        print("Scoreboard:")
+        print("======================================")
+        for user, score in self.points.items():
+            print(f"{user} : {score}", end='\t')
+        print("\n======================================")
